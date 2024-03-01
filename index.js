0ed583e25e4959d8b1fe12648b425da77b568973
amaanraaj <amandwivedi00@gmail.com>
2024-03-02 02:30:36 +0530

    added menu route

--- index.js
+++ index.js
@@ -37,6 +37,30 @@ app.get('/api/swiggy', async (req, res) => {
   }
 });
 
+// Menu Fetch
+app.get('/api/menu', async (req, res) => {
+    const {  lat, lng, restaurantId } = req.query;
+  try {
+    const response = await axios.get(`https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&submitAction=ENTER&lat=${lat}&lng=${lng}&restaurantId=${restaurantId}`, {
+      headers: {
+        // Add any additional headers here
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      }
+    });
+
+    // Ensure the response contains data and is not empty
+    if (!response.data) {
+      throw new Error('Empty response from Swiggy API');
+    }
+
+    // Send the data back to the client
+    res.json(response.data);
+  } catch (error) {
+    console.error('Error fetching data from Swiggy API:', error.message);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
